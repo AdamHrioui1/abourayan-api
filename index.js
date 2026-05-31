bc2cf302a5ba2cd3a4bbf20edecd381173ad0ec9
adamhrioui1 <essaouira024@gmail.com>
2026-05-31 03:21:08 +0100

    edit cors origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://abourayan.netlify.app',
     credentials: true,
 }))
 app.use(cookieParser())
